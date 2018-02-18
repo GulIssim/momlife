b025ce27c70f2c88e56dd750b99a2d6566e6d19e
GulIssim <g.issimova.gi@gmail.com>
2018-02-19 03:02:56 +0600

    changed RequestMapping urls

--- src/main/java/kz/vienna/momlife/controller/EditPatientController.java
+++ src/main/java/kz/vienna/momlife/controller/EditPatientController.java
@@ -7,38 +7,41 @@ import kz.vienna.momlife.repository.PregnancyRepository;
 import kz.vienna.momlife.util.Helper;
 import kz.vienna.momlife.util.IController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 @Controller
 public class EditPatientController extends IController {
+
     @Autowired
     private PatientRepository patientRepository;
     @Autowired
     private PregnancyRepository pregnancyRepository;
 
-    private Patient foundPatient = null;
 
     @RequestMapping(value = "/patient/{patient_id}/pregnancy/{pregnancy_id}/view", method = RequestMethod.POST)
     public String view(@PathVariable("patient_id") int patientId,
                        @PathVariable("pregnancy_id") int pregnancyId,
                        Model model) {
+
         Pregnancy pregnancy = pregnancyRepository.findOne((long) pregnancyId);
         Patient patient = patientRepository.findOne((long) patientId);
+
         model.addAttribute("pregnancy", pregnancy);
         model.addAttribute("patient", patient);
         model.addAttribute("statuses", Helper.getStatuses());
         model.addAttribute("status", "");
+
         return "edit-patient";
     }
 
@@ -49,10 +52,11 @@ public class EditPatientController extends IController {
                                @RequestParam("endTime") String endTime,
                                @RequestParam("status") String statusOption,
                                Model model) {
-        DateFormat df = new SimpleDateFormat("yyyy-mm-dd", Locale.getDefault());
-
         Patient patient = patientRepository.findOne((long) patientId);
         Pregnancy  pregnancy = pregnancyRepository.findOne((long) pregnancyId);
+
+        DateFormat df = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+
         try {
             Date endDate = df.parse(endTime);
             pregnancy.setEnd_time(endDate);
