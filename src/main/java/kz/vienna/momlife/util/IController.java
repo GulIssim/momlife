69eb74160a69ab63cfb1f4d94fba24b60cd07002
GulIssim <g.issimova.gi@gmail.com>
2018-02-19 03:00:39 +0600

    added dateformat string

--- src/main/java/kz/vienna/momlife/util/IController.java
+++ src/main/java/kz/vienna/momlife/util/IController.java
@@ -3,4 +3,5 @@ package kz.vienna.momlife.util;
 public class IController {
     public static final String REDIRECT = "redirect:/";
     public static final String FORWARD = "forward:/";
+    public static final String DATE_FORMAT = "dd-mm-yyyy";
 }
