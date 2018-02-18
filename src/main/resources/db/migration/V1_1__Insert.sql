875205e06dcb89d0bde9bd4716b233dffca3cf14
GulIssim <g.issimova.gi@gmail.com>
2018-02-19 03:00:10 +0600

    added insert sql

---
+++ src/main/resources/db/migration/V1_1__Insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO patient VALUES
+  (1, '123456789123', 'Sit Amet'),
+  (2, '789456123789', 'Lorem Ipsum'),
+  (3, '123456789456', 'Elfor Ramin'),
+  (4, '152846521543', 'Sheden Damir');
+
+
+INSERT INTO pregnancy VALUES
+  (1, 'active', 1, '17-09-2012','17-6-2013'),
+  (2,'success',2, '20-02-2015', '18-11-2015');
